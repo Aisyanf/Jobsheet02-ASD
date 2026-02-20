56aa615bffa7e7d284635e70108c2e2727fe7846
Aisyanf <an.fardani@gmail.com>
2026-02-20 09:28:50 +0700

    Percobaan 2

---
+++ MahasiwaMain02.java
@@ -0,0 +1,17 @@
+public class MahasiwaMain02 {
+    public static void main(String[] args) {
+
+        Mahasiswa02 mhs1 = new Mahasiswa02();
+
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "S1 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("S1 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+    }
+
+}
